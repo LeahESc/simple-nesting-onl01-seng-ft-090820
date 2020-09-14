e68434fd1171d4d0faa20c955726e12c1cd186ae
Leah Schlackman <leahemily1@gmail.com>
2020-09-14 16:51:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -72,9 +72,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     binding.pry
+    # binding.pry
    programmer_hash[:yukihiro_matsumoto] = {}
-   programmer_hash[:yukihiro_matsumoto][:known_for] = " "
+   programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
    programmer_hash[:yukihiro_matsumoto][:languages] = []
    programmer_hash
 end
